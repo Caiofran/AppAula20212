f6d4878adb033e338b9831bcd0d83e8d61d0c9a4
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-08-25 21:11:42 -0300

    add

--- View/Form1.cs
+++ View/Form1.cs
@@ -14,17 +14,41 @@ namespace View
     {
         public Form1()
         {
+            FrmLogin form = new FrmLogin();
+            this.Visible = false;
+            form.ShowDialog();
+            this.Visible = true;
+
             InitializeComponent();
         }
 
         private void btnok_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Você apertou no botão ok");
+            //MessageBox.Show("Você apertou no botão ok");
+            FrmCadPessoa form = new FrmCadPessoa();
+
+            //this.Visible = false;
+
+            //form.Show();
+            form.ShowDialog();
+
+            //this.Visible = true;
+
+            //MessageBox.Show("passou!!");
         }
+
+        /*private void MensagemNaTela(object sender, EventArgs e)
+        {
+            int a = 2;
+            int b = 3;
+            int res = a + b;
+            MessageBox.Show("Resutado " + res);
+        }*/
 
         private void btncancel_Click(object sender, EventArgs e)
         {
 
         }
+
     }
 }
