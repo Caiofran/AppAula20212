7312221c662549ad26c818c1fa9d5f41d3e0f316
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-09-08 20:10:08 -0300

    update

--- View/FrmPrincipal.Designer.cs
+++ View/FrmPrincipal.Designer.cs
@@ -30,40 +30,43 @@ namespace View
         private void InitializeComponent()
         {
             this.barraStatus = new System.Windows.Forms.StatusStrip();
-            this.itbUsuarioLogado = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.itbUsuarioLogado = new System.Windows.Forms.ToolStripStatusLabel();
             this.barraStatus.SuspendLayout();
             this.SuspendLayout();
             // 
             // barraStatus
             // 
+            this.barraStatus.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.barraStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.itbUsuarioLogado});
-            this.barraStatus.Location = new System.Drawing.Point(0, 505);
+            this.barraStatus.Location = new System.Drawing.Point(0, 431);
             this.barraStatus.Name = "barraStatus";
-            this.barraStatus.Size = new System.Drawing.Size(1060, 22);
+            this.barraStatus.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.barraStatus.Size = new System.Drawing.Size(841, 26);
             this.barraStatus.TabIndex = 0;
             this.barraStatus.Text = "statusStrip1";
-            // 
-            // itbUsuarioLogado
-            // 
-            this.itbUsuarioLogado.Name = "itbUsuarioLogado";
-            this.itbUsuarioLogado.Size = new System.Drawing.Size(86, 17);
-            this.itbUsuarioLogado.Text = "usuarioLogado";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(96, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(121, 20);
             this.toolStripStatusLabel1.Text = "Usuário Logado: ";
+            // 
+            // itbUsuarioLogado
+            // 
+            this.itbUsuarioLogado.Name = "itbUsuarioLogado";
+            this.itbUsuarioLogado.Size = new System.Drawing.Size(108, 20);
+            this.itbUsuarioLogado.Text = "usuarioLogado";
             // 
             // FrmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1060, 527);
+            this.ClientSize = new System.Drawing.Size(841, 457);
             this.Controls.Add(this.barraStatus);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmPrincipal";
             this.Text = "Aplicação de Exemplo de Aula";
             this.Load += new System.EventHandler(this.FrmPrincipal_Load);
