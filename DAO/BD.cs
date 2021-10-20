63d730f3fde38a6673477e37c54606150711995e
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-10-20 20:37:06 -0300

    update

---
+++ DAO/BD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public static class BD
+    {
+        //Data Source=D:\Documentos\Faculdade 2021.2\Metodologia e Construção de Projetos de TI\Projetos\AppAula20212\bd.sdf;Password=123
+        public static void AbrirConexao()
+        { }
+
+        public static void FecharConexao()
+        { }
+
+        public static DataTableReader ExecutarSelect(String _SQL)
+        { }
+
+        public static int ExecutarIDU(String _SQL)
+        { }
+
+        public static int ExecutarIDU(String _SQL, List<SqlCeParameter> parameters)
+        { }
+
+        public static Object ExecutarInsertComRetornoDeValor(String _SQL, List<SqlCeParameter> parameters)
+        { }
+    }
+}
